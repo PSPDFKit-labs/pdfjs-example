59aeec2183e06f55f6ec4992df0f34936805cc7d
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2023-03-15 11:41:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## PDF.js Example 
+
+## Getting Started
+
+This repository is an example of how to use [PDF.js](https://mozilla.github.io/pdf.js/). Check out the related [blog post](https://pspdfkit.com/blog/2023/complete-guide-to-pdfjs).
+
+## Setting Up
+
+1. Clone this repository:
+
+```bash
+git clone https://github.com/PSPDFKit-labs/pdfjs-example.git
+```
+
+2. Navigate to the cloned repository:
+
+```bash
+cd pdfjs-example
+```
+
+## Running the Example
+
+1. Install the `serve` package:
+
+```bash
+npm install --global serve
+```
+
+2. Serve the contents of the current directory:
+
+```bash
+serve -l 8080 .
+```
+
+3. Navigate to http://localhost:8080 to view the website.
+
+
+## Contributing
+
+Please ensure [you have signed our CLA](https://pspdfkit.com/guides/web/current/miscellaneous/contributing/) so that we can accept your contributions.
---
+++ index.js
@@ -0,0 +1,80 @@
+// Get the PDF file URL
+const pdfUrl = "./pdf-files/annotation.pdf"
+
+const canvas = document.getElementById("pdf-canvas")
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = "./pdfjs/pdf.worker.js"
+
+// Load the PDF file using PDF.js
+pdfjsLib.getDocument(pdfUrl).promise.then(function (pdfDoc) {
+  pdfDoc
+    .getPage(1)
+    .then(function (page) {
+      const viewport = page.getViewport({ scale: 1 })
+      canvas.width = viewport.width
+      canvas.height = viewport.height
+      const ctx = canvas.getContext("2d")
+      const renderContext = {
+        canvasContext: ctx,
+        viewport: viewport,
+      }
+      page
+        .render(renderContext)
+        .promise.then(function () {
+          console.log("Rendering complete")
+        })
+        .catch(function (error) {
+          console.log("Error rendering page:", error)
+        })
+
+      // Load annotations data
+      page
+        .getAnnotations()
+        .then(function (annotations) {
+          annotations.forEach(function (annotation) {
+            if (annotation.subtype === "Text") {
+              // Render a text annotation
+              const textRect = annotation.rect
+              const text = document.createElement("div")
+              text.style.position = "absolute"
+              text.style.left = textRect[0] + "px"
+              text.style.top = textRect[1] + "px"
+              text.style.width = textRect[2] - textRect[0] + "px"
+              text.style.height = textRect[3] - textRect[1] + "px"
+              text.style.backgroundColor = "green"
+              text.style.opacity = "0.5"
+              text.innerText = annotation.contents
+              canvas.parentNode.appendChild(text)
+            } else if (annotation.subtype === "Highlight") {
+              // Render a highlight annotation
+              const highlightRect = annotation.rect
+              const highlight = document.createElement("div")
+              highlight.style.position = "absolute"
+              highlight.style.left = highlightRect[0] + "px"
+              highlight.style.top = highlightRect[1] + "px"
+              highlight.style.width = highlightRect[2] - highlightRect[0] + "px"
+              highlight.style.height =
+                highlightRect[3] - highlightRect[1] + "px"
+              highlight.style.backgroundColor = "yellow"
+              highlight.style.opacity = "0.5"
+              canvas.parentNode.appendChild(highlight)
+            }
+          })
+        })
+        .catch(function (error) {
+          console.log("Error loading annotations:", error)
+        })
+
+      page.getTextContent().then(function (textContent) {
+        let text = ""
+        for (let i = 0; i < textContent.items.length; i++) {
+          const item = textContent.items[i]
+          text += item.str
+        }
+        console.log(text)
+      })
+    })
+    .catch(function (error) {
+      console.log("Error loading PDF file:", error)
+    })
+})
